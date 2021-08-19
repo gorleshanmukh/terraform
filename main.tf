bb567614535e4184c132e4ad55661e9d691f291b
gorle <249940@aa.com>
2021-08-19 18:53:23 +0530

    backend seperated

--- main.tf
+++ main.tf
@@ -4,10 +4,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "listapp-tf-rg"
-    storage_account_name = "listapptfsa"
-    container_name       = "listappterraformstate"
-    key                  = "listappeastus.tfstate"
+
   }
 }
 
